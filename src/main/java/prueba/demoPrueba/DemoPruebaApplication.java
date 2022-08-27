d7e48c849e96b2fb658a2a84c6c6d2b696fa8733
Roberto Toledo <toledoroberto@gmail.com>
2022-08-26 22:40:10 -0300

    video 4

---
+++ src/main/java/prueba/demoPrueba/DemoPruebaApplication.java
@@ -0,0 +1,13 @@
+package prueba.demoPrueba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoPruebaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoPruebaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/prueba/demoPrueba/DemoPruebaApplicationTests.java
@@ -0,0 +1,13 @@
+package prueba.demoPrueba;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoPruebaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
